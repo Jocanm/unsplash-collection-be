f2f7b66281ff5fcf79cb2b81631325a8cd3788c0
Jose Angarita <amjose09@gmail.com>
2025-07-10 12:23:05 -0500

    changes

--- src/collections/application/use-cases/get-collection-by-id.use-case.ts
+++ src/collections/application/use-cases/get-collection-by-id.use-case.ts
@@ -1,12 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CollectionEntity } from "src/collections/domain/entities/collection.entity";
 import { CollectionRepository } from "src/collections/domain/repositories/collection.repository";
 
 @Injectable()
 export class GetCollectionByIdUseCase {
-  constructor(private readonly collectionRepository: CollectionRepository) {}
+  constructor(private readonly collectionRepository: CollectionRepository) { }
 
-  async execute(id: string): Promise<CollectionEntity | null> {
-    return await this.collectionRepository.findById(id);
+  async execute(id: string): Promise<CollectionEntity> {
+    const collection = await this.collectionRepository.findById(id);
+    if (!collection) {
+      throw new NotFoundException(`Collection with id ${id} not found`)
+    }
+
+    return collection;
   }
 }
